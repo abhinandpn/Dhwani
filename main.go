f0763f96f0bce99de37bb3feaff635df524c277b
abhinandpn <abhinandpn4067@gmail.com>
2025-08-19 10:50:50 +0530

    updated fornet end file parsing

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/abhinandpn/Dhwani/internal/handler"
 	"github.com/abhinandpn/Dhwani/internal/tts"
+	"github.com/abhinandpn/Dhwani/server"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+
+	server.Frontend()
 	// Path to your Google TTS JSON key
 	credsPath := "/home/delta/Downloads/Dhwani-GTTS/dhwani-469106-63cddd3273b0.json"
 
---
+++ server/frontend.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"log"
+	"net/http"
+)
+
+// Frontend serves your static files from the "docs" folder
+func Frontend() {
+	fs := http.FileServer(http.Dir("./docs"))
+	http.Handle("/", fs)
+
+	log.Println("Server running at http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
